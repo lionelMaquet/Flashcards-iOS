ea39eafb1d9cde9c0dc6f7867674f2a12839163e
Lionel <lionel.maquet@gmail.com>
2020-08-22 14:29:02 +0200

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Lionel Maquet on 22/08/2020.
+//  Copyright © 2020 Lionel Maquet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

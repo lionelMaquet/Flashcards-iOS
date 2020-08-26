5d73e7f5f53236f57ade0f4a56f21a06b63feb78
Lionel <lionel.maquet@gmail.com>
2020-08-26 18:50:01 +0200

    adds custom font, resizes icons and categories UI

--- Flashcards/Controllers/CategoryTableViewCell.swift
+++ Flashcards/Controllers/CategoryTableViewCell.swift
@@ -42,26 +42,24 @@ class CategoryTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        titleLabel.textColor = .brown
+        //titleLabel.textColor = 
         
         borderView.layer.borderWidth = 2
         borderView.layer.borderColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1).cgColor
-        borderView.layer.cornerRadius = 35
+        borderView.layer.cornerRadius = 15
+        
+        //Shadows
+        borderView.layer.shadowColor = UIColor.black.cgColor
+        borderView.layer.shadowOpacity = 0.3
+        borderView.layer.shadowOffset = .zero
+        borderView.layer.shadowRadius = 3
     }
 
     @IBAction func deleteCategoryButtonPressed(_ sender: UIButton) {
         
         let alert = UIAlertController(title: "Delete this category", message: "Are you sure you want to delete this deck?", preferredStyle: .alert)
         let okaction = UIAlertAction(title: "delete", style: .default) { (completion) in
-            do {
-                try self.realm.write {
-                    self.realm.delete(self.category!)
-                }
-            } catch {
-                print(error)
-            }
-            
-            self.delegate?.categoryWasDeleted()
+            self.deleteCurrentCategory()
         }
         
         let cancelAction = UIAlertAction(title: "cancel", style: .default) { (completion) in
@@ -76,6 +74,18 @@ class CategoryTableViewCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    func deleteCurrentCategory(){
+        do {
+            try self.realm.write {
+                self.realm.delete(self.category!)
+            }
+        } catch {
+            print(error)
+        }
+        
+        self.delegate?.categoryWasDeleted()
     }
     
 }
